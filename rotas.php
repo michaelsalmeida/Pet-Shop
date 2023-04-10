555de906ccf929886bc709b33d620267edbc2322
michaelsalmeida <michaellsantosalmeida@gmail.com>
2023-04-10 16:13:53 -0300

    Rotas arrumadas

--- rotas.php
+++ rotas.php
@@ -9,30 +9,24 @@ $rootBackPages = $rootFront . "backendPages/";
 $connRoute = $_SERVER['DOCUMENT_ROOT'] . '/Pet-Shop/Backend/conexao.php';
 // $connRoute = $_SERVER['DOCUMENT_ROOT'] . '/backend/conexao.php';
 
+// ----------------------    Rotas do Funcionário    -----------------------------
 
-$loginFunRoute = $rootBackPages . "loginFuncionario.php";
+
+//                           FIM Rotas Funcionário
+
+
+// ----------------------    Rotas do Cliente    -----------------------------
 $loginCliRoute = $rootBackPages . "loginCliente.php";
-$agendamentosRoute = $rootBackPages . "agendamentos.php";
-$cadFunRoute = $rootBackPages . "cadFuncionario.php";
-$registroRoute = $rootBackPages . "registro.php";
-$detalhesRoute = $rootBackPages . "detalhes.php";
-$detalhesHistoricoRoute = $rootBackPages . "detalhesHistorico.php";
-$historicoRoute = $rootBackPages . "historico.php";
-
 $homeRoute = $root . "index.php";
 
-$procLoginFunRoute = $rootBackProc . "proc_loginFun.php";
-$procLoginCliRoute = $rootBackProc . "proc_loginCli.php";
-$procCadFunRoute = $rootBackProc . "proc_cadFuncionario.php";
-$procRegistroRoute = $rootBackProc . "proc_registro.php";
-$procDetalhesRoute = $rootBackProc . "proc_detalhe.php";
-$procFechamentoRoute = $rootBackProc . "proc_fechamento.php";
-$procLogoffRoute = $rootBackProc . "proc_logoff.php";
-$procCalculoRoute = $rootBackProc . "proc_calculo.php";
 
-$confSenhaRoute = $rootBackFunctions . "confsenha.js";
-$modalInfRoute = $rootBackFunctions . "modalInf.js";
-$engineSenhaRoute = $rootBackFunctions . "enginesenha.js";
+//                           FIM Rotas Cliente
+
+
+// ----------------------    Rotas dos Processos    -----------------------------
+$procLoginCliRoute = $rootBackProc . "proc_loginCli.php";
+
+
 $functionsRoute = $rootBackFunctions . "functions.js";
 
 // $verificacaoRoute = $_SERVER['DOCUMENT_ROOT'] . "/Pet-Shop/backend/funcoes/verificacao.php";
