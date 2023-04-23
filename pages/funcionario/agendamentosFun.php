1c8163e4a9d17f9453a77daff1ccd83f5f3ddf14
luizbrito6 <luizg1717@gmail.com>
2023-04-23 11:57:09 -0300

    Header corporativo

--- pages/funcionario/agendamentosFun.php
+++ pages/funcionario/agendamentosFun.php
@@ -16,76 +16,102 @@ require_once $funcoesRoute;
 
 
   <script src="<?php echo $functionsRoute; ?>"></script>
-  <link rel="stylesheet" href="../css-estatico/header.css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="../css-dinamico/table.css">
   <link rel="stylesheet" href="../css-dinamico/funcionario.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css-dinamico/header-corporativo.css">
 </head>
 
 <body onresize="checaDispositivo()" onload="queryBanco('gerarTabelaAgenFun')">
-  <?php
-  if (!loged()) {
-    $_SESSION['msgloginFun'] = "Por favor, faça o login primeiro.";
-    header("Location: " . $loginFunRoute);
-  }
 
-  if (!isset($_SESSION['tipo'])) {
-    // $_SESSION['msgRotaProibidaCli'] = "Você Não possui permissão para entrar nessa página";
-    header("Location: " . $homeRoute);
-  }
+  <header class="header-corporativo">
+    <div class="box-logo-barra-de-pesquisa-perfil">
 
-  if ($_SESSION['tipo'] == 'Secretaria') {
-    echo "<a href=" . $cadastradaDatasRoute . ">Cadastrar horário</a><br><br>";
-    echo "<a href=" . $cadastroCliRoute . ">Cadastrar Cliente</a><br><br>";
-    echo "<a href=" . $agendarParaClienteRoute . ">Agendar consulta</a><br><br>";
-  } elseif ($_SESSION['tipo'] == 'admin') {
-    echo "<a href=" . $cadastrarFunRoute . ">Cadastrar funcionário</a><br><br>";
-    echo "<a href=" . $cadastradaDatasRoute . ">Cadastrar horário</a><br><br>";
-    echo "<a href=" . $cadastroCliRoute . ">Cadastrar Cliente</a><br><br>";
-    echo "<a href=" . $listarFunRoute . ">Listar Funcionários</a><br><br>";
-    echo "<a href=" . $agendarParaClienteRoute . ">Agendar consulta</a><br><br>";
-  }
+      <a href="<?php echo $homeRoute; ?>"><img src="../img-dinamico/logo-corporativo.svg" alt="logo hamtaro petshop corporativo"></a>
 
+      <div class="box-pesquisar">
+        <input type="text" placeholder="Pesquise por um Funcionário" id="pesq">
+        <button onclick="queryBanco('gerarTabelaAgenFun')"><i class="bi bi-search"></i></button>
+      </div>
 
-  if (isset($_SESSION['msgCadData'])) {
-    echo $_SESSION['msgCadData'];
-    unset($_SESSION['msgCadData']);
-  }
+      <div class="perfil-corpotativo">
+        <i class="bi bi-person-square"></i>
+        <p>></p>
+      </div>
 
-  if (isset($_SESSION['msgCadFun'])) {
-    echo $_SESSION['msgCadFun'];
-    unset($_SESSION['msgCadFun']);
-  }
+      <img src="../img-estatico/menu.png" class="menu" alt="menu">
 
-  if (isset($_SESSION['msgRotaProibida'])) {
-    echo $_SESSION['msgRotaProibida'];
-    unset($_SESSION['msgRotaProibida']);
-  }
+    </div>
+
+    <nav class="responsive">
 
 
-  ?>
+      <img src="../img-estatico/fechar.png" class="fechaMenu" alt="fecha">
+
+      <?php
+      if (!loged()) {
+        $_SESSION['msgloginFun'] = "Por favor, faça o login primeiro.";
+        header("Location: " . $loginFunRoute);
+      }
+
+      if (!isset($_SESSION['tipo'])) {
+        // $_SESSION['msgRotaProibidaCli'] = "Você Não possui permissão para entrar nessa página";
+        header("Location: " . $homeRoute);
+      }
+
+      if ($_SESSION['tipo'] == 'Secretaria') {
+        echo "<a href=" . $cadastradaDatasRoute . ">Cadastrar horário</a><br><br>";
+        echo "<a href=" . $cadastroCliRoute . ">Cadastrar Cliente</a><br><br>";
+        echo "<a href=" . $agendarParaClienteRoute . ">Agendar consulta</a><br><br>";
+      } elseif ($_SESSION['tipo'] == 'admin') {
+        echo "<a href=" . $cadastrarFunRoute . ">Cadastrar funcionário</a><br><br>";
+        echo "<a href=" . $cadastradaDatasRoute . ">Cadastrar horário</a><br><br>";
+        echo "<a href=" . $cadastroCliRoute . ">Cadastrar Cliente</a><br><br>";
+        echo "<a href=" . $listarFunRoute . ">Listar Funcionários</a><br><br>";
+        echo "<a href=" . $agendarParaClienteRoute . ">Agendar consulta</a><br><br>";
+      }
+
+
+      if (isset($_SESSION['msgCadData'])) {
+        echo $_SESSION['msgCadData'];
+        unset($_SESSION['msgCadData']);
+      }
+
+      if (isset($_SESSION['msgCadFun'])) {
+        echo $_SESSION['msgCadFun'];
+        unset($_SESSION['msgCadFun']);
+      }
+
+      if (isset($_SESSION['msgRotaProibida'])) {
+        echo $_SESSION['msgRotaProibida'];
+        unset($_SESSION['msgRotaProibida']);
+      }
+
+
+      ?>
+
+
+    </nav>
+
+  </header>
+
 
   <button onclick="executeFunctions('logoff', '')">Sair</button>
 
-  <div class="box-superior">
-
-    <div class="box-opcoes">
-      <select name="status" id="status" onchange="queryBanco('gerarTabelaAgenFun')" required>
-        <option value="" disabled selected hidden>Selecione o status</option>
-        <option value="Disponivel">Disponivel</option>
-        <option value="Marcado">Marcado</option>
-        <option value="Concluido">Concluido</option>
-        <option value="Cancelado">Cancelado</option>
-      </select>
-    </div>
-
-    <div class="box-pesquisar">
-      <input type="text" placeholder="Pesquise por um Funcionário" id="pesq">
-      <button onclick="queryBanco('gerarTabelaAgenFun')"><i class="bi bi-search"></i></button>
-    </div>
-
+    <h1>Agendamentos Marcados</h1>
+  <div class="box-opcoes">
+    <select name="status" id="status" onchange="queryBanco('gerarTabelaAgenFun')" required>
+      <option value="" disabled selected hidden>Selecione o status</option>
+      <option value="Disponivel">Disponivel</option>
+      <option value="Marcado">Marcado</option>
+      <option value="Concluido">Concluido</option>
+      <option value="Cancelado">Cancelado</option>
+    </select>
   </div>
+
+
+
 
   <div class="container box-total">
     <table id="tabela">
@@ -103,6 +129,8 @@ require_once $funcoesRoute;
     </div>
   </form>
 
+
+  <script src="../script.js"></script>
 </body>
 
 </html>
