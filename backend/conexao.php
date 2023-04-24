8ae005c22c4765c58229dec95f6ed7aa3a755ec6
Matheus da Silva Farias <71537027+MatthewsTomts@users.noreply.github.com>
2023-04-24 15:56:26 -0300

    fazer deploy teste

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = "pet-shop-server.mysql.database.azure.com";
-$user = "xjijfzgthm";
-$pass = '8FS407WK3W416425$';
-$dbname = "pet-shop-database";
+$host = "teste1232-server.mysql.database.azure.com";
+$user = "tgtrcuwqye";
+$pass = '571W5M4O7S23FJA0$';
+$dbname = "teste1232-database";
 
 // Criar a conexao com o Banco de Dados
 try {
